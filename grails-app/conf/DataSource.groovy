0dffa26dec62cdc7f552a89ecc30d2b74070dcf2
noel <nmlemus@gmail.com>
2014-12-05 21:29:46 -0200

    MOdification of the mapping names of the tables to table_?{name}. Creation of the DataSourceService and modification of the save function in the DataSourceController.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,10 +1,12 @@
 dataSource {
-    pooled = true
-    jmxExport = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    dbCreate = "create-drop"
+    driverClassName = "org.postgresql.Driver"
+    dialect = "org.hibernate.dialect.PostgreSQLDialect"
+    url = "jdbc:postgresql://localhost:5432/grails_db"
+    username = "postgres"
+    password = "root"
 }
+
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
@@ -17,8 +19,12 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            dbCreate = "update"
+            driverClassName = "org.postgresql.Driver"
+            dialect = "org.hibernate.dialect.PostgreSQLDialect"
+            url = "jdbc:postgresql://localhost:5432/grails_db"
+            username = "postgres"
+            password = "root"
         }
     }
     test {
